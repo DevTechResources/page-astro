6b842dd6863838a5fb353c265bd93526cc0b4e78
Usuario <genericosistem@gmail.com>
2025-04-10 13:28:17 -0500

    testing commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
 export default defineConfig({
-  site: "https://page-atro-acdc.com",
+  site: "https://page-atro-acdc.netlify.app/",
   integrations: [preact()]
 });
