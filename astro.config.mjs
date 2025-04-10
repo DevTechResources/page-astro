4a3da1e2bb40339d1822c2390e4f75b7c58c4e1a
Usuario <genericosistem@gmail.com>
2025-04-10 13:25:05 -0500

    testing commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://page-atro-acdc.com",
   integrations: [preact()]
 });
